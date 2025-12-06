c03495ad8ee332f122533dc6bd36db9e58de37a6
katou <153251950+MiraeNK@users.noreply.github.com>
2025-12-06 20:50:26 +0700

    chore[NK]: remove pnpm-lock to use npm instead

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB7bB7KRDSTYAJx4l0Jp9pmXVkW96odXkQ",
-  authDomain: "mes-simple-store.firebaseapp.com",
-  databaseURL: "https://mes-simple-store-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "mes-simple-store",
-  storageBucket: "mes-simple-store.firebasestorage.app",
-  messagingSenderId: "1077175694210",
-  appId: "1:1077175694210:web:f19b49c6954ee8382c159b",
-  measurementId: "G-RJKX6LS3X9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
